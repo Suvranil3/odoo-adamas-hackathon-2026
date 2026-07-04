49545f93ea8bf272450dfa24cb9f94e1f2698f12
Suvranil3 <ghatak1502@gmail.com>
2026-07-04 12:20:54 +0530

    Harden backend and complete employee module

--- odoo-adamas-hackathon-2026/backend/app.js
+++ odoo-adamas-hackathon-2026/backend/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const employeeRoutes = require('./routes/employeeRoutes');
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use('/employees', employeeRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found'
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+    message: 'Internal Server Error'
+  });
+});
 
 module.exports = app;
